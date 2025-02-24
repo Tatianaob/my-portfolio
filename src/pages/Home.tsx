fb3f3b1f2a31a750f8deabafc8a1863eef356a3f
Tatianaob <ltatiobandore@gmail.com>
2025-02-23 20:02:03 -0800

    Added Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <section className="hero">
+            <h2>Bienvenidos a mi portafolio</h2>
+            <p> Developer | AWS Cloud Practitioner </p>
+        </section>
+    );
+};
+export default Home;
