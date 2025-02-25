8f9eea1bfe7c54377cf42fcc98416fc42937ca16
Tatianaob <ltatiobandore@gmail.com>
2025-02-24 21:14:07 -0800

    Added about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+      <section className="about">
+        <h2>About Me</h2>
+        <p>I am a software developer with expertise in React, TypeScript, and AWS.</p>
+      </section>
+    );
+  };
+  
+  export default About;
+  
