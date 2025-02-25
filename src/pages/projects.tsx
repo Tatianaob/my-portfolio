0ace19ba0465dff8d3eb45703f3b28b282b56f70
Tatianaob <ltatiobandore@gmail.com>
2025-02-24 21:16:37 -0800

    Added page for projects

---
+++ src/pages/projects.tsx
@@ -0,0 +1,15 @@
+const Projects = () => {
+    return (
+      <section className="projects">
+        <h2>My Projects</h2>
+        <ul>
+          <li>Project 1 - Oh-My-Run</li>
+          <li>Project 2 - Weather-Dashboard</li>
+          <li>Project 3 - API-Generator</li>
+        </ul>
+      </section>
+    );
+  };
+  
+  export default Projects;
+  
