2ea625d625c7c97a472f9ccd6fade7b7b5be687f
Tatianaob <ltatiobandore@gmail.com>
2025-02-24 21:18:14 -0800

    Added page for contact info

---
+++ src/pages/contact.tsx
@@ -0,0 +1,12 @@
+const Contact = () => {
+    return (
+      <section className="contact">
+        <h2>Contact Me</h2>
+        <p>Email: ltatiobandore@gmail.com</p>
+        <p>LinkedIn:  </p>
+      </section>
+    );
+  };
+  
+  export default Contact;
+    
